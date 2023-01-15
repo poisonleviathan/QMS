639ffab3fcab08ce96d1be2e8ae058754c6c6b1b
DamithPathirana <damith.pathi3998@gmail.com>
2023-01-15 14:07:36 +0530

    Dashboard init

---
+++ QMS/Dashboard.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace QMS
+{
+    partial class frm_Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Dashboard));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Login_logo_img = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.roundedbtn1 = new QMS.roundedbtn();
+            this.roundedbtn2 = new QMS.roundedbtn();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.roundedbtn3 = new QMS.roundedbtn();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.roundedbtn4 = new QMS.roundedbtn();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Login_logo_img)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 453);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.Login_logo_img);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // Login_logo_img
+            // 
+            this.Login_logo_img.BackColor = System.Drawing.Color.Transparent;
+            this.Login_logo_img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Login_logo_img.Image = ((System.Drawing.Image)(resources.GetObject("Login_logo_img.Image")));
+            this.Login_logo_img.Location = new System.Drawing.Point(64, 2);
+            this.Login_logo_img.Name = "Login_logo_img";
+            this.Login_logo_img.Size = new System.Drawing.Size(61, 98);
+            this.Login_logo_img.TabIndex = 1;
+            this.Login_logo_img.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.roundedbtn1);
+            this.panel3.Location = new System.Drawing.Point(1, 106);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 82);
+            this.panel3.TabIndex = 1;
+            // 
+            // roundedbtn1
+            // 
+            this.roundedbtn1.BackColor = System.Drawing.Color.Transparent;
+            this.roundedbtn1.BackgroundColor = System.Drawing.Color.Transparent;
+            this.roundedbtn1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("roundedbtn1.BackgroundImage")));
+            this.roundedbtn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.roundedbtn1.BorderColor = System.Drawing.Color.Tomato;
+            this.roundedbtn1.BorderRadius = 16;
+            this.roundedbtn1.BorderSize = 0;
+            this.roundedbtn1.FlatAppearance.BorderSize = 0;
+            this.roundedbtn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedbtn1.ForeColor = System.Drawing.Color.White;
+            this.roundedbtn1.Location = new System.Drawing.Point(64, 11);
+            this.roundedbtn1.Name = "roundedbtn1";
+            this.roundedbtn1.Size = new System.Drawing.Size(61, 63);
+            this.roundedbtn1.TabIndex = 0;
+            this.roundedbtn1.TextColor = System.Drawing.Color.White;
+            this.roundedbtn1.UseVisualStyleBackColor = false;
+            // 
+            // roundedbtn2
+            // 
+            this.roundedbtn2.BackColor = System.Drawing.Color.Transparent;
+            this.roundedbtn2.BackgroundColor = System.Drawing.Color.Transparent;
+            this.roundedbtn2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("roundedbtn2.BackgroundImage")));
+            this.roundedbtn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.roundedbtn2.BorderColor = System.Drawing.Color.Tomato;
+            this.roundedbtn2.BorderRadius = 16;
+            this.roundedbtn2.BorderSize = 0;
+            this.roundedbtn2.FlatAppearance.BorderSize = 0;
+            this.roundedbtn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedbtn2.ForeColor = System.Drawing.Color.White;
+            this.roundedbtn2.Location = new System.Drawing.Point(64, 9);
+            this.roundedbtn2.Name = "roundedbtn2";
+            this.roundedbtn2.Size = new System.Drawing.Size(61, 63);
+            this.roundedbtn2.TabIndex = 0;
+            this.roundedbtn2.TextColor = System.Drawing.Color.White;
+            this.roundedbtn2.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Controls.Add(this.roundedbtn2);
+            this.panel4.Location = new System.Drawing.Point(0, 185);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 82);
+            this.panel4.TabIndex = 2;
+            // 
+            // roundedbtn3
+            // 
+            this.roundedbtn3.BackColor = System.Drawing.Color.Transparent;
+            this.roundedbtn3.BackgroundColor = System.Drawing.Color.Transparent;
+            this.roundedbtn3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("roundedbtn3.BackgroundImage")));
+            this.roundedbtn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.roundedbtn3.BorderColor = System.Drawing.Color.Tomato;
+            this.roundedbtn3.BorderRadius = 16;
+            this.roundedbtn3.BorderSize = 0;
+            this.roundedbtn3.FlatAppearance.BorderSize = 0;
+            this.roundedbtn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedbtn3.ForeColor = System.Drawing.Color.White;
+            this.roundedbtn3.Location = new System.Drawing.Point(64, 11);
+            this.roundedbtn3.Name = "roundedbtn3";
+            this.roundedbtn3.Size = new System.Drawing.Size(61, 63);
+            this.roundedbtn3.TabIndex = 0;
+            this.roundedbtn3.TextColor = System.Drawing.Color.White;
+            this.roundedbtn3.UseVisualStyleBackColor = false;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.roundedbtn3);
+            this.panel5.Location = new System.Drawing.Point(0, 78);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(200, 82);
+            this.panel5.TabIndex = 2;
+            // 
+            // roundedbtn4
+            // 
+            this.roundedbtn4.BackColor = System.Drawing.Color.Transparent;
+            this.roundedbtn4.BackgroundColor = System.Drawing.Color.Transparent;
+            this.roundedbtn4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("roundedbtn4.BackgroundImage")));
+            this.roundedbtn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.roundedbtn4.BorderColor = System.Drawing.Color.Tomato;
+            this.roundedbtn4.BorderRadius = 16;
+            this.roundedbtn4.BorderSize = 0;
+            this.roundedbtn4.FlatAppearance.BorderSize = 0;
+            this.roundedbtn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedbtn4.ForeColor = System.Drawing.Color.White;
+            this.roundedbtn4.Location = new System.Drawing.Point(64, 7);
+            this.roundedbtn4.Name = "roundedbtn4";
+            this.roundedbtn4.Size = new System.Drawing.Size(61, 63);
+            this.roundedbtn4.TabIndex = 0;
+            this.roundedbtn4.TextColor = System.Drawing.Color.White;
+            this.roundedbtn4.UseVisualStyleBackColor = false;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.roundedbtn4);
+            this.panel6.Location = new System.Drawing.Point(0, 266);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(200, 82);
+            this.panel6.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(383, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 213);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frm_Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "frm_Dashboard";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dashboard";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Login_logo_img)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox Login_logo_img;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel6;
+        private roundedbtn roundedbtn4;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private roundedbtn roundedbtn3;
+        private roundedbtn roundedbtn2;
+        private roundedbtn roundedbtn1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
